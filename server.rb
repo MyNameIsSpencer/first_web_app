70fba280be124ed4e783b08205d8c28a25379e97
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-16 17:08:05 -0500

    add fav links

--- server.rb
+++ server.rb
@@ -9,5 +9,12 @@ get '/portfolio' do
 end
 
 get '/about' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['art', 'exercise', 'board games','films']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = {'yahoo'=>'https://ca.yahoo.com/?p=us', 'google'=>'https://www.google.ca/', 'facebook'=> 'https://www.facebook.com/', 'bitmaker'=>'https://bitmaker.co/', "eBaum's World" => 'http://www.ebaumsworld.com/'}
+  erb :favourites
 end
